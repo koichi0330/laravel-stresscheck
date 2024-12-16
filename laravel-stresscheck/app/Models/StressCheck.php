65420db5f41f32fdc5899dd38dcae8af9aaecf43
koichi0330 <get7446koichi@gmail.com>
2024-12-16 15:03:12 +0900

    feat: modelの作成

---
+++ laravel-stresscheck/app/Models/StressCheck.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class StressCheck extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'score',
+    ];
+    // Userモデルとのリレーション（StressCheckは1人のUserに関連）
+    public function user()
+    {
+        return $this->belongsTo(User::class); // 1対多の関係
+    }
+    // Questionモデルとのリレーション（StressCheckは1つのQuestionに関連）
+    public function question()
+    {
+        return $this->belongsTo(Question::class); // 1対多の関係
+    }
+}
